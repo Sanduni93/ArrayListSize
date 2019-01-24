930986a252adeaeadf41826a92bb809ee93d2311
Sanduni93 <nirodhajayaweera1993@gmail.com>
2019-01-24 22:21:07 +0530

    Here specifies about size of array list

---
+++ SizeOfArrayList.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+class SizeOfArrayList{
+  
+  public static void main(String args[]){
+
+  	ArrayList<String> laps = new ArrayList<String>();
+
+    laps.add("DELL");
+	laps.add("Asuz");
+	laps.add("Acer");
+	laps.add("Lenovo");
+
+	System.out.println("Size of array list:"+laps.size());
+  }
+
+
+}
